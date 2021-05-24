264cc1ba508757b9c52313e99ce21c071b4efb26
Ida Jørgensen Thinn <ithinn@gmail.com>
2021-05-24 21:16:53 +0200

    Metadata description in Layout

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -4,26 +4,35 @@ import Head from "next/head"
 function Layout( {children, home, maps, animals, facts, games, memory, quiz} ) {
 
     let title = "";
+    let content = "";
 
     if (maps) {
         title = "Kart over regnskogen"
+        content = "Informative kart som viser hvor det fins regnskog i dag, og grunnene til at regnskogen blir borte."
     } else if (animals) {
         title = "Dyr i regnskogen"
+        content = "Les og lær om de rare dyrene i regnskogen."
     } else if (facts) {
         title = "Fakta om regnskogen"
+        content = "Faktaartikler om tropisk regnskog, avskogning, urfolk og menneskene i regnskogen."
     } else if (memory) {
         title = "Memory"
+        content= "Spill det klassiske memory-spillet med motiver fra regnskogen."
     } else if (quiz) {
-        title = "Regnskogsquiz"
+        title = "Regnskogsquiz";
+        content = "Quiz-spill der du skal hjelpe apen Nyani med å klatre til toppen av treet ved å svare riktig på spørsmål"
     } else if (games) {
         title = "Regnskogspill"
+        content = "Quiz og memory-spill med regnskog som tema."
     } else {
         title = "Den fantastiske regnskogen"
+        content = "Utforsk regnskogen ved hjelp av kart, spill og faktaartikler. Passer best for barn mellom 6-10 år, og dekker flere læreplanmål."
     }
 
     return(
         <>  
         <Head>
+            <meta name="description" content={content}/>
             <meta name="viewport" lang="no" content="minimum-scale=1, initial-scale=1, width=device-width" />
             <title>{title}</title>
             <link rel="shortcut icon" type="image/svg" href="./img/icons/home_home.svg"/>
